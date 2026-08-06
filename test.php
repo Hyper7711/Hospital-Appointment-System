99d662a2faa5ca41ba1ec0904b548fa813fe12f8
Sneha1730 <yamgarsneha0@gmail.com>
2026-08-06 12:06:27 +0530

    php_base_done

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+
+include "db.php";
+
+if ($conn) {
+    echo "<h2 style='color:green;'>Database Connected Successfully ✅</h2>";
+} else {
+    echo "<h2 style='color:red;'>Connection Failed ❌</h2>";
+}
+
+?>
