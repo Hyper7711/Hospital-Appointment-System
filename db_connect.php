9a90b15235bbc6733642fe85cd43b91dd0b5aae8
Hyper7711 <shindeyash054@gmail.com>
2026-08-08 11:13:31 +0530

    Port_updated

--- db_connect.php
+++ db_connect.php
@@ -4,8 +4,9 @@ $host = "localhost";
 $user = "root";
 $password = "";
 $database = "hospital_opd_db";
+$port = 3307;
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
